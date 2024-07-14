d2b7e641b3cec66e0698260557bf5ee22d8c3fde
Julien Goethal <116565257+Ju-the-king@users.noreply.github.com>
2024-07-14 19:59:21 +0200

    test

--- simple_graph.py
+++ simple_graph.py
@@ -3,4 +3,18 @@ import matplotlib.pyplot as plt
 import plotly
 
 
+def plot_data(database):
+    """
+    This function is a test to see if we can use the code in different branches
+    :param database : database we are using
+    :return:
+    """
 
+    data = database[:5][:]
+    player = data["Player"]
+    points = data["PTS"]
+    mp = data["MP"]
+    for i in range(5):
+        plt.plot(mp[i], points[i])
+        plt.annotate(player[i], xy=(mp[i], points[i]))
+    plt.show()
